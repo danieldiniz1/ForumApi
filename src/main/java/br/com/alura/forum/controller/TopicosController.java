cdd79cedb255c6c65047074c15792d8ad68828e5
Daniel Diniz <daniel.diniz1@icloud.com>
2022-02-20 21:57:20 -0300

    trabalhando refatorando e adicionando rota para novos tópicos

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,31 +1,49 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.model.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping("/nome")
     public List<TopicoDTO> listaPeloNome(String nomeCurso) {
         if (nomeCurso == null){
             return TopicoDTO.converter(topicoRepository.findAll());
         }
         return TopicoDTO.converter(topicoRepository.findByCursoNome(nomeCurso));
+     }
+
+     @GetMapping("/id")
+    public String listaPeloId(Long id){
+        return  topicoRepository.findById(id).get().getTitulo();
+     }
+
+     @PostMapping("/cadastro")
+     public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder){
+        Topico topico  = topicoForm.converter(cursoRepository);
+        topicoRepository.save(topico);
+
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
      }
 
 
