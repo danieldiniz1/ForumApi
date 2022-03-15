0ffb93ced7a901c2044500db144c6c70e7e8e258
Daniel Diniz <daniel.diniz1@icloud.com>
2022-03-15 17:43:30 -0300

    adicionando imagen dockerfile

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module forum {
+    requires guava;
+    requires jjwt;
+    requires spring.beans;
+    requires spring.boot.autoconfigure;
+    requires spring.boot.devtools;
+    requires spring.context;
+    requires spring.data.commons;
+    requires spring.data.jpa;
+    requires spring.web;
+    requires springfox.core;
+    requires springfox.spi;
+    requires springfox.spring.web;
+    requires springfox.swagger2;
+}
