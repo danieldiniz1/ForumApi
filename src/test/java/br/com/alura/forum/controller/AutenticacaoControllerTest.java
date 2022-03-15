326b53aaffcc21358d742d0d0c50e660c6b9a243
Daniel Diniz <daniel.diniz1@icloud.com>
2022-03-15 16:17:47 -0300

    testando repository e criando BD auxiliar

---
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -0,0 +1,11 @@
+package br.com.alura.forum.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class AutenticacaoControllerTest {
+
+    @Test
+    private void verificaEMailESenhaEFazLogin(){
+
+    }
+}
--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -6,17 +6,26 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void deveCarregarUmCursoAoBuscarPeloNome(){
         String nomeCurso = "HTML 5";
+
+        Curso html5 = new Curso(nomeCurso,"Programacao");
+        em.persist(html5);
         Curso curso = cursoRepository.findByNome(nomeCurso);
         Assertions.assertNotNull(curso);
         Assertions.assertEquals(nomeCurso, curso.getNome());
