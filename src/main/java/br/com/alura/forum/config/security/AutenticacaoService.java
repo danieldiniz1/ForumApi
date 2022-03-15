70151eefbc0729f3c413a892f7f19b86e918f715
Daniel Diniz <daniel.diniz1@icloud.com>
2022-03-15 12:32:48 -0300

    separando profile dev and prod

--- src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
@@ -3,6 +3,7 @@ package br.com.alura.forum.config.security;
 import br.com.alura.forum.model.Usuario;
 import br.com.alura.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
--- src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -18,8 +18,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AutenticacaoService autenticacaoService;
@@ -46,22 +46,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-//                .antMatchers(HttpMethod.GET, "/actuator").permitAll() // em negócios reais, ele não é aberto (aqui é só para testar o actuator)
-//                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll() // em negócios reais, ele não é aberto (aqui é só para testar o actuator)
-                .antMatchers(HttpMethod.POST,"/auth").permitAll()
-                .antMatchers(HttpMethod.DELETE,"/topicos/*").hasRole("MODERADOR")
-                .anyRequest().authenticated()
-                .and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService,usuarioRepository), UsernamePasswordAuthenticationFilter.class);
-
+                .antMatchers("/**").permitAll()
+                .and().csrf().disable();
     }
 
-    // configuração de recursos estaticos(js, css, imagens, swagger)
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**","/swagger-resources/**");
-    }
 }
